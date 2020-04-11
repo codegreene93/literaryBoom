86778c8f72668ea1ed8125b8d65df4d5a4e0ed72
Ubuntu <ubuntu@ip-172-31-33-37.ec2.internal>
2020-04-11 18:35:07 +0000

    registeruser

--- config/routes.rb
+++ config/routes.rb
@@ -4,34 +4,20 @@ Rails.application.routes.draw do
   get 'cart/index'
 
  get '/home' => 'static_pages#home'
- 
  get '/about' => 'static_pages#about'
- 
  get '/help' => 'static_pages#help'
  
- get '/login' => 'static_pages#login'
+ get '/register' => 'registrations#new'
+ get '/login' => 'sessions#new'
  
- get '/register' => 'static_pages#register'
- 
- get '/profile' => 'static_pages#profile'
  
  get '/cart/clear', to: 'cart#clear'
- 
  get '/cart', to: 'cart#index'
- 
  get '/cart/:id', to: 'cart#add'
- 
  get '/cart/remove/:id' => 'cart#remove'
- 
  get '/cart/decrease/:id' => 'cart#decrease'
  
   root 'static_pages#home'
- 
- # get 'static_pages/home'
- # get 'static_pages/about'
- # get 'static_pages/help'
- # get 'static_pages/login'
- # get 'static_pages/register'
 
   resources :posts
   devise_for :users, controllers: {registrations: "registrations"}
