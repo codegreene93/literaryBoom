00e4c19fff65356c6563a8b9a2a15e9c82f64989
Ubuntu <ubuntu@ip-172-31-33-37.ec2.internal>
2020-04-16 15:28:58 +0000

    revert stackoberflow changes

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Deviseapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
     
  ActionMailer::Base.smtp_settings = {
       :address => 'smtp.gmail.com',
