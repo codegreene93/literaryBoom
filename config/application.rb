7c223f86a5e5e3c093e80f213c289b399e9c91f5
Ubuntu <ubuntu@ip-172-31-33-37.ec2.internal>
2020-04-16 14:09:57 +0000

    deploy commit 3

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,8 @@
 require_relative 'boot'
 
-require 'rails/all'
-
 config.assets.initialize_on_precompile = false
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
